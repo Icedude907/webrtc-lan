ac31413387359be429bbf8f18807da33cc04e8f8
Icedude907 <34080011+Icedude907@users.noreply.github.com>
2024-10-06 18:59:37 +1300

    Support client name changes

--- src/usersession.rs
+++ src/usersession.rs
@@ -1,10 +1,11 @@
-use std::sync::Mutex;
+use std::{future::Future, sync::Mutex};
 
 use bytes::Bytes;
 use derive_more::derive::{Deref, Display};
 use just_webrtc::types::PeerConnectionState;
 use lazy_static::lazy_static;
 use log::{info, warn};
+use tokio::sync::broadcast;
 
 use crate::{chatroom::CHAT, packets::{self, Encode}, util::UUIDGen, webrtcpeer::{ClientConnection, RecvError}};
 
@@ -21,43 +22,20 @@ impl ActiveSession{
     pub fn new(conn: ClientConnection)->Self{
         Self{conn, user: UserSession::new()}
     }
-    pub async fn handle_active_session(self){
+    pub async fn handle_active_session(mut self){
         let (mut broadcast, send2everyone) = CHAT.lock().unwrap().add_participant(&self.user.username);
-
-        let handle_incoming = |msg: Bytes|->_{
-            use packets::PktC2S::*;
-            info!("{} >> {:?}", self.user.username, msg);
-            let Ok(pkt) = packets::decode(msg.to_vec()) else {
-                warn!("{} connection error", self.user.username);
-                return Err(())
-            };
-            info!("\t{:?}", pkt);
-            match pkt{
-                SendMsg(p)=>{
-                    let Ok(_) = send2everyone.send(p.msg) else {return Err(())};
-                }
-                _ => {},
-            }
-            return Ok(())
-        };
-        let handle_outgoing = |msg: String|->_{
-            let msg = packets::PktS2C_ReceiveMsg::new(msg).encode();
-            let bytes = msg.into();
-            // info!("{} << {:?}", self.user.username, bytes);
-            self.send(bytes)
-        };
 
         loop{tokio::select! {
             c2s = self.conn.recv() => match c2s{
                 // Receive data
-                Ok(msg)=>{handle_incoming(msg);},
+                Ok(msg)=>{self.handle_incoming(msg, &send2everyone).await;},
                 // Connection shutdown
                 Err(RecvError::Abort)=>{ break; }
                 Err(_)=>{ warn!("Unexpected error. Closing connection."); break; }
             },
             s2c = broadcast.recv() => match s2c{
                 Ok(msg)=>{
-                    match handle_outgoing(msg).await {
+                    match self.handle_outgoing(msg).await {
                         Ok(_) => {},
                         Err(x) => { warn!("Error: Could not send to client {}", x); break; }
                     }
@@ -82,6 +60,36 @@ impl ActiveSession{
             _ => {}
         }
         return Ok(())
+    }
+    async fn handle_incoming(&mut self, msg: Bytes, send2everyone: &broadcast::Sender<String>)->Result<(),()>{
+        use packets::PktC2S::*;
+        info!("{} >> {:?}", self.user.username, msg);
+        let Ok(pkt) = packets::decode(msg.to_vec()) else {
+            warn!("{} connection error", self.user.username);
+            return Err(())
+        };
+        info!("\t{:?}", pkt);
+        match pkt{
+            SendMsg(p)=>{
+                let _ = send2everyone.send(p.msg);
+            }
+            SetName(p)=>{
+                // Send approval
+                self.send(packets::PktS2C_SetNameReply::new(p.name.clone()).encode().into()).await;
+                if self.user.username != p.name {
+                    let _ = send2everyone.send(format!(">>> {} is now {}", self.user.username, p.name));
+                    self.user.username = p.name;
+                }
+            }
+            _ => {},
+        }
+        return Ok(())
+    }
+    async fn handle_outgoing(&self, msg: String)->Result<usize, just_webrtc::platform::Error>{
+        let msg = packets::PktS2C_ReceiveMsg::new(msg).encode();
+        let bytes = msg.into();
+        // info!("{} << {:?}", self.user.username, bytes);
+        self.send(bytes).await
     }
 }
 
